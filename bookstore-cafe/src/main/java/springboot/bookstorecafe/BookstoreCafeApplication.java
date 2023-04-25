07640dc33712b8412e6e1ff784dcd49e24daca70
MarcinPilarz <130612@student.uni.opole.pl>
2023-04-25 21:05:53 +0200

    develop added spring project
    Added basic project in spring

---
+++ bookstore-cafe/src/main/java/springboot/bookstorecafe/BookstoreCafeApplication.java
@@ -0,0 +1,13 @@
+package springboot.bookstorecafe;
+
+import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreCafeApplication.class, args);
+	}
+
+}
