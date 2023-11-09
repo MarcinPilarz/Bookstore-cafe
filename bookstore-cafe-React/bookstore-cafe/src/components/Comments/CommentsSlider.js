2fdd555d9aa9d0c371f61ce15c926cad6f14d06a
MarcinPilarz <130612@student.uni.opole.pl>
2023-11-09 22:26:32 +0100

    HomePage styling CommentsSection

--- bookstore-cafe-React/bookstore-cafe/src/components/Comments/CommentsSlider.js
+++ bookstore-cafe-React/bookstore-cafe/src/components/Comments/CommentsSlider.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import "./CommentsSlider.css";
 const CommentsSlider = () => {
   const [reviews, setReviews] = useState([]);
   const [page, setPage] = useState(1);
@@ -17,7 +17,7 @@ const CommentsSlider = () => {
       });
   }, []);
 
-  const numberOfComments = 4;
+  const numberOfComments = 3;
   const numberOfPage = Math.ceil(reviews.length / numberOfComments);
 
   const indexStart = (page - 1) * numberOfComments;
@@ -32,21 +32,27 @@ const CommentsSlider = () => {
     console.log("cośtam", currentComments);
   };
   return (
-    <div>
+    <div className="comments pagination">
       {Array.from({ length: numberOfPage }, (_, i) => (
-        <span key={`page-${i + 1}`} onClick={() => handlePageClick(i + 1)}>
+        <span
+          key={`page-${i + 1}`}
+          onClick={() => handlePageClick(i + 1)}
+          className={i + 1 === page ? "active" : ""}
+        >
           {i + 1}
         </span>
       ))}
-      <div>
+      <div className="comments-all-window">
         {currentComments.map((review, index) => (
-          <div key={index}>
-            <h3>{review.rating}/5</h3>
-            <p>{review.reviewData}</p>
-            <p>{review.reviewContent}</p>
-            <p>
+          <div className="comment-window" key={index}>
+            <p className="data">{review.reviewData}</p>
+            <h3 className="rating">{review.rating}/5</h3>
+
+            <p className="review-content">{review.reviewContent}</p>
+            <p className="name-person">
               {review.person.firstName} {review.person.lastName}
             </p>
+            <img className="image-person" src="photo.jpg"></img>
           </div>
         ))}
       </div>
