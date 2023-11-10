6ad77524e5b0973c8f458b9f497e5472dac7ae98
MarcinPilarz <130612@student.uni.opole.pl>
2023-11-10 13:14:51 +0100

    HomePage adding history icon in NavBar.js

--- bookstore-cafe-React/bookstore-cafe/src/components/Navbar/Navbar.js
+++ bookstore-cafe-React/bookstore-cafe/src/components/Navbar/Navbar.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBasketShopping, faBars } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBasketShopping,
+  faBars,
+  faClock,
+} from "@fortawesome/free-solid-svg-icons";
 import { BusketSideBar } from "../SideBars/BusketSideBar";
 import { useLocation } from "react-router-dom";
 import "./Navbar.css";
@@ -51,6 +55,7 @@ const Navbar = () => {
         <div onClick={toggleMenu} className="hamburger">
           <FontAwesomeIcon icon={faBars} />
         </div>
+
         <ul className="navbar">
           <li>
             {" "}
@@ -76,35 +81,43 @@ const Navbar = () => {
           </li>
         </ul>
 
-        <div className="busket-icon" onClick={busketBar}>
-          {" "}
-          <FontAwesomeIcon icon={faBasketShopping} />{" "}
-        </div>
-        <div className={`busket-sidebar${!busketOrderBar ? "openBusket" : ""}`}>
-          {busketOrderBar && (
-            <div className="order-item-list">
-              <p>
-                <b>Nazwa produktu:</b>
-                <br />
-                Americano{" "}
-              </p>
-              <p>
-                <b>Ilość: </b>
-                <br />2{" "}
-              </p>
-              <p>
-                <b>Cena: </b>
-                <br />
-                40zł{" "}
-              </p>
-            </div>
-          )}
-          {busketOrderBar && (
-            <div className="button-container">
-              <button className="summary-button">Podsumowanie</button>
-              <button className="clear-button">Wyczyść</button>
-            </div>
-          )}
+        <div className="icon-container">
+          <div className="busket-icon" onClick={busketBar}>
+            {" "}
+            <FontAwesomeIcon icon={faBasketShopping} />{" "}
+          </div>
+          <div
+            className={`busket-sidebar${!busketOrderBar ? "openBusket" : ""}`}
+          >
+            {busketOrderBar && (
+              <div className="order-item-list">
+                <p>
+                  <b>Nazwa produktu:</b>
+                  <br />
+                  Americano{" "}
+                </p>
+                <p>
+                  <b>Ilość: </b>
+                  <br />2{" "}
+                </p>
+                <p>
+                  <b>Cena: </b>
+                  <br />
+                  40zł{" "}
+                </p>
+              </div>
+            )}
+            {busketOrderBar && (
+              <div className="button-container">
+                <button className="summary-button">Podsumowanie</button>
+                <button className="clear-button">Wyczyść</button>
+              </div>
+            )}
+          </div>
+
+          <div className="history-icon">
+            <FontAwesomeIcon icon={faClock} />
+          </div>
         </div>
       </nav>
     </header>
